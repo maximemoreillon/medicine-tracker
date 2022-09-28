70aa9b631754440be7d88cb04bab7c80bb2a552d
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-28 13:16:00 +0900

    errro handling

--- controllers/intakes.ts
+++ controllers/intakes.ts
@@ -1,17 +1,34 @@
 import Medicine from '../models/medicine'
+import CreateHttpError from 'http-errors'
 import { Request, Response, NextFunction } from 'express'
 
 export const create_medicine_intake = async (req: Request, res: Response, next: NextFunction) => {
-    const { _id } = req.params
-    const medicine = await Medicine.findById(_id)
-    // TODO: use HTTP-errors
-    if (!medicine) return res.status(404).send('Medicine not found')
-    medicine.intake.push({ date: new Date() })
-    await medicine.save()
-    console.log(`Medicine ${_id} intake added`)
-    res.send(medicine)
+    try {
+        const { _id } = req.params
+        const medicine = await Medicine.findById(_id)
+        if (!medicine) throw CreateHttpError(404, 'Medicine not found') 
+        medicine.intake.push({ date: new Date() })
+        await medicine.save()
+        console.log(`Medicine ${_id} intake added`)
+        res.send(medicine)
+    } catch (error) {
+        next(error)
+    }
+    
 }
 
 export const delete_medicine_intake = async (req: Request, res: Response, next: NextFunction) => {
-    res.status(501).send('Not implemented')
+    try {
+        const { _id, intake_id} = req.params
+        const medicine = await Medicine.findById(_id)
+        if (!medicine) throw CreateHttpError(404, 'Medicine not found')
+        const foundIndex = medicine.intake.findIndex(i => i._id?.toString() === intake_id)
+        if (!medicine) throw CreateHttpError(404, 'Intake not found')
+        medicine.intake.splice(foundIndex, 1)
+        await medicine.save()
+        console.log(`Medicine ${_id} intake deleted`)
+        res.send(medicine)
+    } catch (error) {
+        next(error)
+    }
 }
--- controllers/medicines.ts
+++ controllers/medicines.ts
@@ -1,38 +1,61 @@
 import Medicine from '../models/medicine'
+import CreateHttpError from 'http-errors'
 import { Request, Response, NextFunction} from 'express'
 
 
 export const create_medicine = async (req: Request, res: Response, next: NextFunction) => {
-    const { user } = res.locals
-    const properties = { ...req.body, user_id: user._id}
-    const newMedicine = await Medicine.create(properties)
-    console.log(`Medicine ${newMedicine._id} created`)
-    res.send(newMedicine)
+    try {
+        const { user } = res.locals
+        const properties = { ...req.body, user_id: user._id }
+        const newMedicine = await Medicine.create(properties)
+        console.log(`Medicine ${newMedicine._id} created`)
+        res.send(newMedicine)
+    } catch (error) {
+        next(error)
+    }
+    
 }
 
 export const read_medicines = async (req: Request, res: Response, next: NextFunction) => {
-    const { user } = res.locals
-    const medicines = await Medicine.find({user_id: user._id})
-    console.log(`Medicines queried`)
-    res.send(medicines)
+    try {
+        const { user } = res.locals
+        const medicines = await Medicine.find({ user_id: user._id })
+        console.log(`Medicines queried`)
+        res.send(medicines)
+    } catch (error) {
+        next(error)
+    }
+    
 }
 
 export const read_medicine = async (req: Request, res: Response, next: NextFunction) => {
-    const { _id } = req.params
-    const medicine = await Medicine.findById(_id)
-    console.log(`Medicine ${_id} queried`)
-    res.send(medicine)
+    try {
+        const { _id } = req.params
+        const medicine = await Medicine.findById(_id)
+        if (!medicine) throw CreateHttpError(404, 'Medicine not found') 
+        console.log(`Medicine ${_id} queried`)
+        res.send(medicine)
+    } catch (error) {
+        next(error)
+    }
+    
 }
 
 export const update_medicine = async (req: Request, res: Response, next: NextFunction) => {
+    
     res.status(501).send('not implemented')
 }
 
 export const delete_medicine = async (req: Request, res: Response, next: NextFunction) => {
-    const {_id} = req.params
-    const medicine = await Medicine.findByIdAndDelete(_id)
-    // Todo: check if actually found
-    console.log(`Medicine ${_id} deleted`)
-    res.send(medicine)
+    try {
+        const { _id } = req.params
+        const medicine = await Medicine.findByIdAndDelete(_id)
+        // Todo: check if actually found
+        console.log(`Medicine ${_id} deleted`)
+        res.send(medicine)
+    } catch (error) {
+        next(error)
+    }
+    
 }
 
